acd39ff3fbf59310f866c1e48b89f5d54e38c601
blombergandreas <andreas.blomberg@yubico.com>
2021-05-11 14:43:20 +0200

    Update after security review

--- Scripts/YubiHSM_Cert_Enroll.sh
+++ Scripts/YubiHSM_Cert_Enroll.sh
@@ -10,12 +10,13 @@ usage () {
 	echo "-d, --domain                Domain(s) for the new key [Default: 1]"
 	echo "-a, --algorithm             Algorithm to use for the new key [Default: RSA2048]"
 	echo "-i, --authkeyid             Key ID of the Authentication Key used [Default: 0x0001]"
-	echo "-p, --authpassword          Password for the Authentication Key [Default: password]"
-	echo "-c, --cacertificate         CA certificate to use [Default: ./rootCA-Cert.pem]"
-	echo "-s, --caprivatekey          Privet key for the CA certificate [Default: ./rootCA-Key.pem]"
+	echo "-p, --authpassword          Password for the Authentication Key [Default: ]"
+	echo "-c, --cacertificate         CA certificate to use [Default: ./TestCACert.pem]"
+	echo "-s, --caprivatekey          Privet key for the CA certificate [Default: ./TestCAKey.pem]"
 	echo "-r, --caprivatekeypw        Password for the CA certificate privet file [Default: ]"
 	echo "-f, --pkcs11configfile      PKCS11 configuration file [Default: ./sun_yubihsm2_pkcs11.conf]"
 	echo "-o, --dname                 X.500 Distinguished Name to be used as subject fields [Default: ]"
+	echo "-t, --logfile               Log file path [Default: ./YubiHSM_PKCS11_Enroll.log"
 	echo "-q, --quiet                 Suppress output"
 	echo ""
 	echo "  Example: `basename "$0"` -k 0x0002 -n MyKey -d 1 -a rsa2048 -i 0x0001 -p password -c ./rootCA-Cert.pem -s ./rootCA-Key.pem -f ./sun_yubihsm2_pkcs11.conf"
@@ -26,82 +27,81 @@ PrintMessages() {
 	local msg="$1"
 	local Silent="$2"
 	if [ "$Silent" != true ] ; then
-    echo $msg
+    	echo $msg
 	fi
 }
-	
 
 PrintErrorAndExit () {
 	FunctionFailed="$1"
 	ExitCode="$2"
 	>&2 echo -e "\e[31m$FunctionFailed failed with exit code $ExitCode\e[39m"
-	>&2 echo -e "\e[31mSee $logfile for more information\e[39m"
+	>&2 echo -e "\e[31mSee $LogFile for more information\e[39m"
 	exit 1 
 }
 
 ConvertToDER () {
-  local Infile="$1"
-  local Outfile="$2"
-  openssl x509 -in "$Infile" -out "$Outfile" -outform DER >> $logfile 2>&1
-  local ExitCode=$?
+	local Infile="$1"
+	local Outfile="$2"
+	openssl x509 -in "$Infile" -out "$Outfile" -outform DER >> $LogFile 2>&1
+	local ExitCode=$?
 	if [ $ExitCode -ne 0 ]
 	then
-	  PrintErrorAndExit "DeleteOpaque" $ExitCode
+		PrintErrorAndExit "DeleteOpaque" $ExitCode
 	fi
 }
 
 DeleteOpaque () {
-  local Password="$1"
-  local AuthKey_ID="$2"
-  local Key_ID="$3"
-  yubihsm-shell -p "$Password" --authkey="$AuthKey_ID" -a delete-object -i "$Key_ID" -t opaque >> $logfile 2>&1
-  local ExitCode=$?
+	local Password="$1"
+	local AuthKey_ID="$2"
+	local Key_ID="$3"
+	yubihsm-shell -p "$Password" --authkey="$AuthKey_ID" -a delete-object -i "$Key_ID" -t opaque >> $LogFile 2>&1
+	local ExitCode=$?
 	if [ $ExitCode -ne 0 ]
 	then
-	  PrintErrorAndExit "DeleteOpaque" $ExitCode
+		PrintErrorAndExit "DeleteOpaque" $ExitCode
 	fi
 }
 
 PutOpaque () {
-  local Password="$1"
-  local AuthKey_ID="$2"
-  local Key_ID="$3"
-  local Key_Name="$4"
-  local Domain="$5"
-  local Infile="$6"
-  yubihsm-shell -p "$Password" --authkey="$AuthKey_ID" -a put-opaque -i "$Key_ID" -d "$Domain" -l "$Key_Name" -A opaque-x509-certificate --in "$Infile" >> $logfile 2>&1 
-  local ExitCode=$?
+	local Password="$1"
+	local AuthKey_ID="$2"
+	local Key_ID="$3"
+	local Key_Name="$4"
+	local Domain="$5"
+	local Infile="$6"
+	yubihsm-shell -p "$Password" --authkey="$AuthKey_ID" -a put-opaque -i "$Key_ID" -d "$Domain" -l "$Key_Name" -A opaque-x509-certificate --in "$Infile" >> $LogFile 2>&1 
+	local ExitCode=$?
 	if [ $ExitCode -ne 0 ]
 	then
-	  PrintErrorAndExit "PutOpaque" $ExitCode
+		PrintErrorAndExit "PutOpaque" $ExitCode
 	fi
 }
 
 SignAttestationCertificate () {
-  local Password="$1"
-  local AuthKey_ID="$2"
-  local Key_ID="$3"
-  local Outfile="$4"
-  yubihsm-shell -p "$Password" --authkey="$AuthKey_ID" -a sign-attestation-certificate -i "$Key_ID" --attestation-id 0 --out "$Outfile" >> $logfile 2>&1
-  local ExitCode=$?
-	if [ $ExitCode -ne 0 ]
-	then
-	  PrintErrorAndExit "SignAttestationCertificate" $ExitCode   
-  fi
-}
-
-GenerateKeyPair () {
-  local Password="$1"
-  local AuthKey_ID="$2"
-  local Key_ID="$3"
-  local Key_Name="$4"
-  local Domain="$5"
-  local Algorithm="$6"
-	yubihsm-shell -p "$Password" --authkey="$AuthKey_ID" -a generate-asymmetric-key -c sign-pkcs,sign-pss,sign-attestation-certificate -A "$Algorithm" -l "$KeyName" -d "$Domain" -i "$KeyID" >> $logfile 2>&1
+	local Password="$1"
+	local AuthKey_ID="$2"
+	local Key_ID="$3"
+	local Outfile="$4"
+	yubihsm-shell -p "$Password" --authkey="$AuthKey_ID" -a sign-attestation-certificate -i "$Key_ID" --attestation-id 0 --out "$Outfile" >> $LogFile 2>&1
 	local ExitCode=$?
 	if [ $ExitCode -ne 0 ]
 	then
-	  PrintErrorAndExit "GenerateKeyPair" $ExitCode
+		PrintErrorAndExit "SignAttestationCertificate" $ExitCode   
+	fi
+}
+
+GenerateKeyPair () {
+	local Password="$1"
+	local AuthKey_ID="$2"
+	local Key_ID="$3"
+	local Key_Name="$4"
+	local Domain="$5"
+	local Algorithm="$6"
+	yubihsm-shell -p "$Password" --authkey="$AuthKey_ID" -a generate-asymmetric-key -c sign-pkcs,sign-pss,sign-attestation-certificate -A "$Algorithm" -l "$KeyName" -d "$Domain" -i "$KeyID" >> $LogFile 2>&1
+	local ExitCode=$?
+	if [ $ExitCode -ne 0 ]
+	then
+		PrintErrorAndExit "GenerateKeyPair" $ExitCode
 	fi
 }
 
@@ -113,14 +113,14 @@ CreateAndExportCSR () {
 	local D_name="$5"
 	if [ -z "$D_name" ]
 	then
-		keytool -certreq -alias "$Key_Name" -sigalg SHA256withRSA -file "$CSRFile" -keystore NONE -storetype PKCS11 -providerClass sun.security.pkcs11.SunPKCS11 -providerarg "$PKCS11Conf_File" -storepass "$Store_PW" -v >> $logfile 2>&1
+		keytool -certreq -alias "$Key_Name" -sigalg SHA256withRSA -file "$CSRFile" -keystore NONE -storetype PKCS11 -providerClass sun.security.pkcs11.SunPKCS11 -providerarg "$PKCS11Conf_File" -storepass "$Store_PW" -v >> $LogFile 2>&1
 	else
-		keytool -certreq -alias "$Key_Name" -sigalg SHA256withRSA -file "$CSRFile" -keystore NONE -storetype PKCS11 -providerClass sun.security.pkcs11.SunPKCS11 -providerarg "$PKCS11Conf_File" -storepass "$Store_PW" -dname "$D_name" -v >> $logfile 2>&1
+		keytool -certreq -alias "$Key_Name" -sigalg SHA256withRSA -file "$CSRFile" -keystore NONE -storetype PKCS11 -providerClass sun.security.pkcs11.SunPKCS11 -providerarg "$PKCS11Conf_File" -storepass "$Store_PW" -dname "$D_name" -v >> $LogFile 2>&1
 	fi
 	local ExitCode=$?
 	if [ $ExitCode -ne 0 ]
 	then
-	  PrintErrorAndExit "CreateAndExportCSR" $ExitCode
+		PrintErrorAndExit "CreateAndExportCSR" $ExitCode
 	fi
 }
 
@@ -137,24 +137,24 @@ SignCertificate () {
 	fi
 	if [ -z "$CAPrivateKeyPW" ]
 	then
-		  openssl x509 -req -in "$CSRFile" -CA "$CA_Cert" -CAkey "$CA_Key" -CAcreateserial -out "$SignedCertFile" -sha256 -days 365 >> $logfile 2>&1
+		  openssl x509 -req -in "$CSRFile" -CA "$CA_Cert" -CAkey "$CA_Key" -CAcreateserial -out "$SignedCertFile" -sha256 -days 365 >> $LogFile 2>&1
 	else 
-		  openssl x509 -req -in "$CSRFile" -CA "$CA_Cert" -CAkey "$CA_Key" -CAcreateserial -out "$SignedCertFile" -sha256 -passin pass:"$CAPrivateKey_PW" -days 365 >> $logfile 2>&1
+		  openssl x509 -req -in "$CSRFile" -CA "$CA_Cert" -CAkey "$CA_Key" -CAcreateserial -out "$SignedCertFile" -sha256 -passin pass:"$CAPrivateKey_PW" -days 365 >> $LogFile 2>&1
 	fi
-  local ExitCode=$?
+	local ExitCode=$?
 	if [ $ExitCode -ne 0 ]
 	then
-	  PrintErrorAndExit "SignCertificate" $ExitCode
+		PrintErrorAndExit "SignCertificate" $ExitCode
 	fi
 }
 
 Cleanup () {
 	local Tempdir="$1"
-	rm -R "$Tempdir" >> $logfile 2>&1
+	rm -R "$Tempdir" >> $LogFile 2>&1
 	local ExitCode=$?
 	if [ $ExitCode -ne 0 ]
 	then
-	  PrintErrorAndExit "Cleanup" $ExitCode
+		PrintErrorAndExit "Cleanup" $ExitCode
 	fi
 }
 
@@ -163,68 +163,73 @@ while [[ $# > 0 ]]
 do
 	case "$1" in
     -k|--keyid)
-      KeyID="$2"
-      shift
-      ;;
+		KeyID="$2"
+		shift
+		;;
 
-		-n|--keyname)
-			KeyName="$2"
-			shift
-			;;
+	-n|--keyname)
+		KeyName="$2"
+		shift
+		;;
 			
     -d|--domain)
-      Domain="$2"
-      shift
-      ;;
+		Domain="$2"
+		shift
+		;;
 
-		-a|--algorithm) 
-      Algorithm="$2"
-      shift
-      ;;            
+	-a|--algorithm) 
+		Algorithm="$2"
+		shift
+		;;            
 
-		-i|--authkeyid) 
-      AuthKeyID="$2"
-      shift
-      ;;            
+	-i|--authkeyid) 
+		AuthKeyID="$2"
+		shift
+		;;            
 
-		-p|--authpassword)
-      AuthPW="$2"
-      shift
-      ;;            
+	-p|--authpassword)
+		AuthPW="$2"
+		shift
+		;;            
 
-		-c|--cacertificate)
-      CACertificate="$2"
-      shift
-      ;;
+	-c|--cacertificate)
+		CACertificate="$2"
+		shift
+		;;
       
-		-s|--caprivatekey)
-      CAPrivateKey="$2"
-      shift
-      ;;      
+	-s|--caprivatekey)
+		CAPrivateKey="$2"
+		shift
+		;;      
 
-		-r|--caprivatekeypw)
-      CAPrivateKeyPW="$2"
-      shift
-      ;;
+	-r|--caprivatekeypw)
+		CAPrivateKeyPW="$2"
+		shift
+		;;
 		
-		-f|--pkcs11configfile)
-      PKCS11ConfFile="$2"
-      shift
-      ;; 
+	-f|--pkcs11configfile)
+		PKCS11ConfFile="$2"
+		shift
+		;; 
 
     -o|--dname)
-      Dname="$2"
-      shift
-      ;;    
+		Dname="$2"
+		shift
+		;;    
 
     -q|--quiet)
-      Quiet="true"
-      ;;             
-      		
+		Quiet="true"
+		;;
+	
+	-t|--logfile)
+		LogFile="$2"
+		shift
+		;;
+		
     *)
-			usage
-			exit 1
-			;;
+		usage
+		exit 1
+		;;
 			
 	esac
 	shift
@@ -236,45 +241,25 @@ KeyName="${KeyName:-MyKey1}"
 Domain="${Domain:-1}"
 Algorithm="${Algorithm:-RSA2048}"
 AuthKeyID="${AuthKeyID:-0x0001}"
-AuthPW="${AuthPW:-password}"
-CACertificate="${CACertificate:-./rootCA-Cert.pem}"
-CAPrivateKey="${CAPrivateKey:-./rootCA-Key.pem}"
+AuthPW="${AuthPW:-}"
+CACertificate="${CACertificate:-./TestCACert.pem}"
+CAPrivateKey="${CAPrivateKey:-./TestCAKey.pem}"
 PKCS11ConfFile="${PKCS11ConfFile:-./sun_yubihsm2_pkcs11.conf}"
 CAPrivateKeyPW="${CAPrivateKeyPW:-}"
 Dname="${Dname:-}"
+LogFile="${LogFile:-./YubiHSM_PKCS11_Setup.log}"
 Quiet="${Quiet:-false}"
 
 
 # Work variables
-logfile='./YubiHSM_PKCS11_Setup.log'
 temp_dir=$(mktemp -d)
 TemplateCert=$(mktemp $temp_dir/TemplateCert.XXXXXXXXXXXX)
-SelfsignedCert=$(mktemp $temp_dir/SelfsignedCert.XXXXXXXXXXXX)
 SignedCert=$(mktemp $temp_dir/SignedCert.XXXXXXXXXXXX)
 CSR=$(mktemp $temp_dir/YHSM2-Sig.XXXXXXXXXXXX.csr)
 PEMext='.pem'
 DERext='.der'
 StorePW=$(echo $AuthKeyID | sed 's/0x//')$AuthPW
 
-PrintMessages "Will use the following settings to create the java code signing certificate on yubiHSM" $Quiet
-PrintMessages "-----" $Quiet
-PrintMessages "Key ID: $KeyID" $Quiet
-PrintMessages "Key name: $KeyName" $Quiet
-PrintMessages "Domain: $Domain" $Quiet
-PrintMessages "Algorithm: $Algorithm" $Quiet
-PrintMessages "Auth key ID: $AuthKeyID" $Quiet
-PrintMessages "Auth password: $AuthPW" $Quiet
-PrintMessages "CA certificate: $CACertificate" $Quiet
-PrintMessages "CA private key: $CAPrivateKey" $Quiet
-PrintMessages "Java PKCS11 configuration file: $PKCS11ConfFile" $Quiet
-PrintMessages "CA private key password: $CAPrivateKeyPW" $Quiet
-if [ -z "$Dname" ]
-then
-	PrintMessages "X.500 Distinguished Name: CN=YubiHSM Attestation id:$KeyID" $Quiet
-else
-		PrintMessages "X.500 Distinguished Name: $Dname" $Quiet
-fi
-PrintMessages "-----" $Quiet
 ##### Main program #####
 
 # Generate the RSA key-pair
@@ -293,22 +278,6 @@ ConvertToDER "$TemplateCert$PEMext" "$TemplateCert$DERext"
 PrintMessages "Import template certificate" $Quiet
 PutOpaque "$AuthPW" "$AuthKeyID" "$KeyID" "$KeyName" "$Domain" "$TemplateCert$DERext"
 
-# Generate a self-signed attestation certificate
-PrintMessages "Generate a self-signed attestation certificate" $Quiet
-SignAttestationCertificate "$AuthPW" "$AuthKeyID" "$KeyID" "$SelfsignedCert$PEMext"
-
-# Convert to DER format
-PrintMessages "Convert self-signed certificate to DER format" $Quiet
-ConvertToDER "$SelfsignedCert$PEMext" "$SelfsignedCert$DERext"
-
-# Delete template certificate on the YubiHSM
-PrintMessages "Delete template certificate on the YubiHSM" $Quiet
-DeleteOpaque "$AuthPW" "$AuthKeyID" "$KeyID"
-
-# Import self-signed certificate
-PrintMessages "Import self-signed certificate" $Quiet
-PutOpaque "$AuthPW" "$AuthKeyID" "$KeyID" "$KeyName" "$Domain" "$SelfsignedCert$DERext"
-
 # Create and export the CSR
 PrintMessages "Create and export a CSR" $Quiet
 CreateAndExportCSR "$CSR" "$KeyName" "$PKCS11ConfFile" "$StorePW" "$Dname"
@@ -319,7 +288,7 @@ PrintMessages "Sign the Java code signing certificate" $Quiet
 SignCertificate "$CSR" "$CACertificate" "$CAPrivateKey" "$SignedCert$PEMext" "$CAPrivateKeyPW"
 
 # Delete the self-signed certificate on YubiHSM"
-PrintMessages "Delete the self-signed certificate on YubiHSM" $Quiet
+PrintMessages "Delete the template certificate on YubiHSM" $Quiet
 DeleteOpaque "$AuthPW" "$AuthKeyID" "$KeyID"
 
 # Convert signed certificate to DER format
