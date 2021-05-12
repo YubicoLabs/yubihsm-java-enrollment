a6bfb3e570c752b3eba601c6a93741fcfd49f79f
blombergandreas <andreas.blomberg@yubico.com>
2021-05-12 22:13:17 +0200

    bug fixes and security improvement

--- Scripts/YubiHSM_Cert_Enroll.ps1
+++ Scripts/YubiHSM_Cert_Enroll.ps1
@@ -4,18 +4,25 @@ param (
     [string]$KeyName = 'MyKey1',
     [string]$WorkDirectory = (Get-Item .).FullName,
     [string]$Domain = '1', 
-    [String]$AuthKeyID = '0x0001',
-    [String]$AuthPW = "",
+    [string]$AuthKeyID = '0x0001',
+    [parameter(Mandatory=$true)]
+    [string]$AuthPW = "",
     [string]$CAcertificate = 'TestCACert.pem',
     [string]$CAPrivateKey = 'TestCAKey.pem',
-    [String]$CAPrivateKeyPW = '',
+    [string]$CAPrivateKeyPW = '',
     [string]$PKCS11Config = 'sun_yubihsm2_pkcs11.conf',
-    [string]$Dname = '', # format 'CN=cName, OU=orgUnit, O=org, L=city, S=state, C=countryCode' 
-    [String]$LogFile = (Join-Path -Path $WorkDirectory -ChildPath 'YubiHSM_PKCS11_Enroll.log'),
-    [Switch]$Quiet
+    [string]$Dname = '',
+    [string]$LogFile = (Join-Path -Path $WorkDirectory -ChildPath 'YubiHSM_PKCS11_Enroll.log'),
+    [switch]$Quiet
 )
 
-$StorePW = $AuthKeyID.Replace("0x","") + $AuthPW
+[securestring]$SAuthPW = ConvertTo-SecureString $AuthPW -AsPlainText -Force
+Remove-Variable AuthPW
+if ($CAPrivateKeyPW -ne '') {
+    [securestring]$SCAPrivateKeyPW=ConvertTo-SecureString $CAPrivateKeyPW -AsPlainText -Force
+    Remove-Variable CAPrivateKeyPW
+}
+[securestring]$SStorePW = ConvertTo-SecureString $($AuthKeyID.Replace("0x","") + $(ConvertFrom-SecureString $SAuthPW -AsPlainText)) -AsPlainText -Force
 $PEMext = '.pem'
 $DERext = '.der'
 $Template_cert = Join-Path -Path $WorkDirectory -ChildPath 'template_cert'
@@ -34,7 +41,6 @@ function PrintMessages {
         Write-Output $Messages    
     }
 }
-
 
 function PrintErrorAndExit {
     param (
@@ -61,11 +67,11 @@ function ConvertToDER {
 
 function DeleteOpaque {
      param (
-        $Password,
+        [securestring]$Password,
         $AuthKey_ID,
         $Key_ID
     )
-    yubihsm-shell -p $Password --authkey=$AuthKey_ID -a delete-object -i $Key_ID -t opaque 2>&1 >> $Logfile
+    yubihsm-shell -p $(ConvertFrom-SecureString $Password -AsPlainText) --authkey=$AuthKey_ID -a delete-object -i $Key_ID -t opaque 2>&1 >> $Logfile
     if( $LASTEXITCODE -ne 0 ) {
         PrintErrorAndExit -FunctionFailed "DeleteOpaque" -ErrorCode $LASTEXITCODE 
     }
@@ -73,14 +79,14 @@ function DeleteOpaque {
 
 function PutOpaque {
         param (
-        $Password,
+        [securestring]$Password,
         $AuthKey_ID,
         $Key_ID,
         $Key_Name,
         $Domains,
         $Infile
     )
-    yubihsm-shell -p $Password --authkey=$AuthKey_ID -a put-opaque -i $Key_ID -d $Domains -l $Key_Name -A opaque-x509-certificate --in $Infile 2>&1 >> $Logfile
+    yubihsm-shell -p $(ConvertFrom-SecureString $Password -AsPlainText) --authkey=$AuthKey_ID -a put-opaque -i $Key_ID -d $Domains -l $Key_Name -A opaque-x509-certificate --in $Infile 2>&1 >> $Logfile
     if( $LASTEXITCODE -ne 0 ) {
         PrintErrorAndExit -FunctionFailed "PutOpaque" -ErrorCode $LASTEXITCODE 
     }
@@ -88,12 +94,12 @@ function PutOpaque {
 
 function SignAttestationCertificate {
     param (
-        $Password,
+        [securestring]$Password,
         $AuthKey_ID,
         $Key_ID,
         $Outfile
     )
-    yubihsm-shell -p $Password --authkey=$AuthKey_ID -a sign-attestation-certificate -i $KeyID --attestation-id 0 --out $Outfile 2>&1 >> $Logfile
+    yubihsm-shell -p $(ConvertFrom-SecureString $Password -AsPlainText) --authkey=$AuthKey_ID -a sign-attestation-certificate -i $KeyID --attestation-id 0 --out $Outfile 2>&1 >> $Logfile
     if( $LASTEXITCODE -ne 0 ) {
         PrintErrorAndExit -FunctionFailed "SignAttestationCertificate" -ErrorCode $LASTEXITCODE 
     }
@@ -109,14 +115,14 @@ function Cleanup {
 
 function GenerateKeyPair {
     param (
-        $Password,
+        [securestring]$Password,
         $AuthKey_ID,
         $Algo,
         $Key_name,
         $Domains,
         $Key_ID
     )
-    yubihsm-shell -p $Password --authkey=$AuthKey_ID -a generate-asymmetric-key -c sign-pkcs,sign-pss,sign-attestation-certificate -A $Algo -l $Key_name -d $Domains -i $Key_ID 2>&1 >> $Logfile
+    yubihsm-shell -p $(ConvertFrom-SecureString $Password -AsPlainText) --authkey=$AuthKey_ID -a generate-asymmetric-key -c sign-pkcs,sign-pss,sign-attestation-certificate -A $Algo -l $Key_name -d $Domains -i $Key_ID 2>&1 >> $Logfile
     if( $LASTEXITCODE -ne 0 ) {
         PrintErrorAndExit -FunctionFailed "GenerateKeyPair" -ErrorCode $LASTEXITCODE 
     }
@@ -127,13 +133,13 @@ function CreateAndExportCSR {
         $KeyAlias,
         $CSRFile,
         $PKCS11ConfigFile,
-        $StorePassword,
+        [securestring]$StorePassword,
         $D_name
     )
     if ( $D_name -eq '') {
-        keytool -certreq -alias "$KeyAlias" -sigalg SHA256withRSA -file "$CSRFile" -keystore NONE -storetype PKCS11 -providerClass sun.security.pkcs11.SunPKCS11 -providerarg "$PKCS11ConfigFile" -storepass "$StorePassword" -v 2>&1 >> $Logfile
+        keytool -certreq -alias "$KeyAlias" -sigalg SHA256withRSA -file "$CSRFile" -keystore NONE -storetype PKCS11 -providerClass sun.security.pkcs11.SunPKCS11 -providerarg "$PKCS11ConfigFile" -storepass $(ConvertFrom-SecureString $StorePassword -AsPlainText) -v 2>&1 >> $Logfile
     } else {
-        keytool -certreq -alias "$KeyAlias" -sigalg SHA256withRSA -file "$CSRFile" -keystore NONE -storetype PKCS11 -providerClass sun.security.pkcs11.SunPKCS11 -providerarg "$PKCS11ConfigFile" -storepass "$StorePassword" -dname "$D_name" -v 2>&1 >> $Logfile
+        keytool -certreq -alias "$KeyAlias" -sigalg SHA256withRSA -file "$CSRFile" -keystore NONE -storetype PKCS11 -providerClass sun.security.pkcs11.SunPKCS11 -providerarg "$PKCS11ConfigFile" -storepass $(ConvertFrom-SecureString $StorePassword -AsPlainText) -dname "$D_name" -v 2>&1 >> $Logfile
     }
     if( $LASTEXITCODE -ne 0 ) {
         PrintErrorAndExit -FunctionFailed "CreateAndExportCSR" -ErrorCode $LASTEXITCODE 
@@ -145,13 +151,13 @@ function SignCertificate {
         $CSRFile,
         $CA_Cert,
         $CA_Key,
-        $CAPrivateKeyPassword,
+        [securestring]$CAPrivateKeyPassword,
         $SignedCertFile
     )
-    if ( $CAPrivateKeyPassword -eq '') {
+    if ( $null -eq $CAPrivateKeyPassword) {
         openssl x509 -req -in "$CSRFile" -CA "$CA_Cert" -CAkey "$CA_Key" -CAcreateserial -out "$SignedCert$PEMext" -days 500 -sha256 2>&1 >> $Logfile
     } else {
-        openssl x509 -req -in "$CSRFile" -CA "$CA_Cert" -CAkey "$CA_Key" -CAcreateserial -out "$SignedCert$PEMext" -days 500 -sha256 -passin pass:"$CAPrivateKeyPassword" 2>&1 >> $Logfile
+        openssl x509 -req -in "$CSRFile" -CA "$CA_Cert" -CAkey "$CA_Key" -CAcreateserial -out "$SignedCert$PEMext" -days 500 -sha256 -passin "pass:$(ConvertFrom-SecureString $CAPrivateKeyPassword -AsPlainText)" 2>&1 >> $Logfile
     }
     if( $LASTEXITCODE -ne 0 ) {
         PrintErrorAndExit -FunctionFailed "SignCertificate" -ErrorCode $LASTEXITCODE 
@@ -165,11 +171,11 @@ Write-Output "Started $((Get-Date).ToString())" 2>&1 >> $Logfile
 
 # Generate the RSA key-pair
 PrintMessages -Messages "Generate the RSA key-pair" -Silent $Quiet
-GenerateKeyPair -Password "$AuthPW" -AuthKey_ID "$AuthKeyID" -Algo "$Algorithm" -Key_name "$KeyName" -Domains "$Domain" -Key_ID "$KeyID"
+GenerateKeyPair -Password $SAuthPW -AuthKey_ID "$AuthKeyID" -Algo "$Algorithm" -Key_name "$KeyName" -Domains "$Domain" -Key_ID "$KeyID"
 
 # Create a template certificate
 PrintMessages -Messages  "Creating a template certificate" -Silent $Quiet
-SignAttestationCertificate -Password "$AuthPW" -AuthKey_ID "$AuthKeyID" -Key_ID "$KeyID" -Outfile "$Template_cert$PEMext"
+SignAttestationCertificate -Password $SAuthPW -AuthKey_ID "$AuthKeyID" -Key_ID "$KeyID" -Outfile "$Template_cert$PEMext"
 
 # Convert to DER format
 PrintMessages -Messages "Convert template certificate to DER format" -Silent $Quiet
@@ -177,20 +183,20 @@ ConvertToDER -Infile "$Template_cert$PEMext" -Outfile "$Template_cert$DERext"
 
 # Import template certificate
 PrintMessages -Messages "Import template certificate" $Quiet
-PutOpaque -Password "$AuthPW" -AuthKey_ID "$AuthKeyID" -Key_ID "$KeyID" -Key_Name "$KeyName" -Domains "$Domain" -Infile "$Template_cert$DERext"
+PutOpaque -Password $SAuthPW -AuthKey_ID "$AuthKeyID" -Key_ID "$KeyID" -Key_Name "$KeyName" -Domains "$Domain" -Infile "$Template_cert$DERext"
 
 # Create and export the CSR
 PrintMessages -Messages  "Create and export a CSR" -Silent $Quiet
-CreateAndExportCSR -KeyAlias "$KeyName" -CSRFile "$CSR" -PKCS11ConfigFile "$PKCS11ConfFile" -StorePassword "$StorePW" -D_name "$Dname"
+CreateAndExportCSR -KeyAlias "$KeyName" -CSRFile "$CSR" -PKCS11ConfigFile "$PKCS11ConfFile" -StorePassword $SStorePW -D_name "$Dname"
 
 # Sign the Java code signing certificate
 # This step uses OpenSSL CA as an example
 PrintMessages -Messages  "Sign the Java code signing certificate" -Silent $Quiet
-SignCertificate -CSRFile "$CSR" -CA_Cert "$CACert" -CA_Key "$CAKey" -SignedCertFile "$SignedCert$PEMext" -CAPrivateKeyPassword "$CAPrivateKeyPW" 
+SignCertificate -CSRFile "$CSR" -CA_Cert "$CACert" -CA_Key "$CAKey" -SignedCertFile "$SignedCert$PEMext" -CAPrivateKeyPassword $SCAPrivateKeyPW
 
 # Delete the template certificate on YubiHSM"
 PrintMessages -Messages  "Delete the template certificate on YubiHSM" -Silent $Quiet
-DeleteOpaque -Password "$AuthPW" -AuthKey_ID "$AuthKeyID" -Key_ID "$KeyID"
+DeleteOpaque -Password $SAuthPW -AuthKey_ID "$AuthKeyID" -Key_ID "$KeyID"
 
 # Convert signed certificate to DER format
 PrintMessages -Messages  "Convert signed certificate to DER format" -Silent $Quiet
@@ -198,7 +204,7 @@ ConvertToDER -Infile "$SignedCert$PEMext" -Outfile "$SignedCert$DERext"
 
 # Import the Java code signing certificate
 PrintMessages -Messages "Import the Java code signing certificate" -Silent $Quiet
-PutOpaque -Password "$AuthPW" -AuthKey_ID "$AuthKeyID" -Key_ID "$KeyID" -Key_Name "$KeyName" -Domains "$Domain" -Infile "$SignedCert$DERext"
+PutOpaque -Password $SAuthPW -AuthKey_ID "$AuthKeyID" -Key_ID "$KeyID" -Key_Name "$KeyName" -Domains "$Domain" -Infile "$SignedCert$DERext"
 
 # Remove temporary files
 PrintMessages -Messages "Remove temporary files" $Quiet
